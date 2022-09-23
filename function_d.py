df412321bfbe7bc118d775683165bd8fa4bb3792
bin/bash -c "$(curl -fsSL https://gist.githubusercontent.com/CheezItMan/e31aebdb0f686c1a194e980b24f3cea4/raw/5710e04d17a7840df3df0ea95502da275a9943cb/ada_c14_installfest.bash) <svetikyss89gmail.com>
2022-09-23 11:14:46 -0700

    Function_d

--- function_d.py
+++ function_d.py
@@ -5,6 +5,19 @@ def max_value(numbers):
     max_number = max(numbers)
     return max_number
 
+requency_index = {}
+    max_frequency = -1
+    most_common_value = None
+    for num in number_list:
+        if frequency_index.get(num):
+            frequency_index[num] += 1
+        else:
+            frequency_index[num] = 1
 
+        if max_frequency < frequency_index[num]:
+            max_frequency = frequency_index[num]
+            most_common_value = num
+
+    return most_common_value
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
